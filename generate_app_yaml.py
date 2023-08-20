9d72e0a7e659f3e7ef632a6e7fd8a4e3feea95f5
melabady <me@xpay.app>
2023-08-20 18:23:19 +0300

    try to get service name from env variable

--- generate_app_yaml.py
+++ generate_app_yaml.py
@@ -1,3 +1,6 @@
+import os
+
+branchname = os.getenv('BRANCH_NAME') 
 #input file
 fin = open("app.yaml.temp", "rt")
 #output file to write the result to
@@ -5,7 +8,7 @@ fout = open("app.yaml", "wt")
 #for each line in the input file
 for line in fin:
 	#read replace the string and write to output file
-	fout.write(line.replace('$BRANCH_NAME', '123'))
+	fout.write(line.replace('$BRANCH_NAME', 'branchname'))
 #close input and output files
 fin.close()
 fout.close()
