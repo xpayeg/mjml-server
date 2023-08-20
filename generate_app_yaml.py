0feb121b573edddef855de1fa9839d717147235a
melabady <me@xpay.app>
2023-08-20 18:44:59 +0300

    try to get service name from env variables

--- generate_app_yaml.py
+++ generate_app_yaml.py
@@ -1,14 +1,14 @@
 import os
 
 file_name = "app.yaml"
-branch_name = os.getenv('BRANCH_NAME') 
+repo_name = os.getenv('REPO_NAME') 
 
 file = open(file_name, "r")
 
 data = file.read()
 file.close()
 
-data = data.replace("BRANCH_NAME", branch_name)
+data = data.replace("REPO_NAME", repo_name)
 
 file = open(file_name, "w")
 file.write(data)
